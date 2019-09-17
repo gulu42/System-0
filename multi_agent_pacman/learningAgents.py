6b1bd7e771df7fdb7fc5e540899abb941cc57244
gulu42 <aditya.gulati@iiitb.org>
2019-09-17 23:25:37 +0530

    set epsilon to zero in system 1

--- multi_agent_pacman/learningAgents.py
+++ multi_agent_pacman/learningAgents.py
@@ -4,7 +4,7 @@
 # educational purposes provided that (1) you do not distribute or publish
 # solutions, (2) you retain this notice, and (3) you provide clear
 # attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
-# 
+#
 # Attribution Information: The Pacman AI projects were developed at UC Berkeley.
 # The core projects and autograders were primarily created by John DeNero
 # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
@@ -185,6 +185,9 @@ class ReinforcementAgent(ValueEstimationAgent):
     ################################
     def setEpsilon(self, epsilon):
         self.epsilon = epsilon
+
+    def getEpsilon(self):
+        return self.epsilon
 
     def setLearningRate(self, alpha):
         self.alpha = alpha
